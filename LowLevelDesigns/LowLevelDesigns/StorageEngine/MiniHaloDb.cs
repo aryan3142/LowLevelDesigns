c3b552d0179b23d1444249dfbf130a3abd47ee8c
arykhand <aryan.khandelwal@gartner.com>
2025-03-17 10:14:07 +0530

    Added feature for handling concurrent read/write operations and WALs

--- LowLevelDesigns/LowLevelDesigns/StorageEngine/MiniHaloDb.cs
+++ LowLevelDesigns/LowLevelDesigns/StorageEngine/MiniHaloDb.cs
@@ -9,7 +9,9 @@ namespace LowLevelDesigns.StorageEngine
     public class MiniHaloDb
     {
         private readonly string _dataFilePath = "data.db";
+        private readonly string _walFilePath = "wal.log";
         private Dictionary<string, long> _index = new();
+        private ReaderWriterLockSlim _lock = new();
 
         public MiniHaloDb()
         {
@@ -18,69 +20,106 @@ namespace LowLevelDesigns.StorageEngine
                 File.Create(_dataFilePath);
 
             // Rebuild index from existing file
+            RecoverFromWal();
             RebuildIndex();
         }
 
         // Save key-value to file and update index
         public void Put(string key, string value)
         {
-            // Initialize a new instance of FileStream class with specified path, creation mode and read-write access
-            using var fileStream = new FileStream(_dataFilePath, FileMode.Append, FileAccess.Write);
-            
-            // Gets the current position of this stream
-            long offset = fileStream.Position;
+            try
+            {
+                _lock.EnterWriteLock();
+                AppendToWal(key,value);
 
-            // Encodes all the character in a specified string to a sequence of bytes
-            // WHy is it required ?
-            // Disk files are binary, not text, string needs to be converted to byte arrays for binary writing. We are using UTF-8
-            // encoding which is standard and supports all characters.
+                // Initialize a new instance of FileStream class with specified path, creation mode and read-write access
+                using var fileStream = new FileStream(_dataFilePath, FileMode.Append, FileAccess.Write);
 
-            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
-            byte[] valueBytes = Encoding.UTF8.GetBytes(value);
+                // Gets the current position of this stream
+                long offset = fileStream.Position;
 
-            // When reading later we need to know:
-            // How many bytes to read for the key
-            // How many bytes to read for the value
-            // BitConverter.GetBytes(int) converts the length to a 4-byte binary format.
-            fileStream.Write(BitConverter.GetBytes(keyBytes.Length));
-            fileStream.Write(BitConverter.GetBytes(valueBytes.Length));
-            fileStream.Write(keyBytes);
-            fileStream.Write(valueBytes);
+                // Encodes all the character in a specified string to a sequence of bytes
+                // WHy is it required ?
+                // Disk files are binary, not text, string needs to be converted to byte arrays for binary writing. We are using UTF-8
+                // encoding which is standard and supports all characters.
 
-            // Now that lengths are written we can safely store the data
-            // Later when reading,
-            // You'll first read first 4 bytes for key length
-            // Then read 4 bytes for value length
-            // then read key length bytes for the key
-            // and value length bytes for the value
-            // Final format: [keylength: 4 bytes for key][valuelength: 4 bytes of value][key (Nbytes][value (mbytes)]
-            _index[key] = offset;
+                byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+                byte[] valueBytes = Encoding.UTF8.GetBytes(value);
+
+                // When reading later we need to know:
+                // How many bytes to read for the key
+                // How many bytes to read for the value
+                // BitConverter.GetBytes(int) converts the length to a 4-byte binary format.
+                fileStream.Write(BitConverter.GetBytes(keyBytes.Length));
+                fileStream.Write(BitConverter.GetBytes(valueBytes.Length));
+                fileStream.Write(keyBytes);
+                fileStream.Write(valueBytes);
+
+                // Now that lengths are written we can safely store the data
+                // Later when reading,
+                // You'll first read first 4 bytes for key length
+                // Then read 4 bytes for value length
+                // then read key length bytes for the key
+                // and value length bytes for the value
+                // Final format: [keylength: 4 bytes for key][valuelength: 4 bytes of value][key (Nbytes][value (mbytes)]
+                _index[key] = offset;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        private void AppendToWal(string key, string value)
+        {
+            using var writer = new StreamWriter(_walFilePath, true);
+            writer.Write($"PUT|{key}|{value}");
         }
 
         // Read value using offset for index
         public string Get(string key)
         {
-            if (!_index.ContainsKey(key)) return null;
+            _lock.EnterReadLock();
+            try
+            {
+                if (!_index.ContainsKey(key)) return null;
 
-            long offset =_index[key];
-            using var fileStream = new FileStream(_dataFilePath, FileMode.Append, FileAccess.Read);
-            fileStream.Seek(offset, SeekOrigin.Begin);
+                long offset = _index[key];
+                using var fileStream = new FileStream(_dataFilePath, FileMode.Append, FileAccess.Read);
+                fileStream.Seek(offset, SeekOrigin.Begin);
 
-            byte[] intBuffer = new byte[4];
+                byte[] intBuffer = new byte[4];
 
-            fileStream.Read(intBuffer, 0, 4);
-            int keyLen = BitConverter.ToInt32(intBuffer);
+                fileStream.Read(intBuffer, 0, 4);
+                int keyLen = BitConverter.ToInt32(intBuffer);
 
-            fileStream.Read(intBuffer, 0, 4);
-            int valLen = BitConverter.ToInt32(intBuffer);
+                fileStream.Read(intBuffer, 0, 4);
+                int valLen = BitConverter.ToInt32(intBuffer);
 
-            byte[] keyBuffer = new byte[keyLen];
-            fileStream.Read(keyBuffer, 0, keyLen);
+                byte[] keyBuffer = new byte[keyLen];
+                fileStream.Read(keyBuffer, 0, keyLen);
 
-            byte[] valueBuffer = new byte[valLen];
-            fileStream.Read(valueBuffer, 0, valLen);
+                byte[] valueBuffer = new byte[valLen];
+                fileStream.Read(valueBuffer, 0, valLen);
 
-            return Encoding.UTF8.GetString(valueBuffer);
+                return Encoding.UTF8.GetString(valueBuffer);
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        private void RecoverFromWal()
+        {
+            foreach(var line in File.ReadLines(_walFilePath))
+            {
+                var parts = line.Split("|");
+                if (parts[0] == "PUT")
+                {
+                    Put(parts[1], parts[2]);
+                }
+            }
         }
 
         // Rebuild the index from the data file
